3098c1a876a2e39edaf5c9f2f82781472e3032f7
Aljoscha Rheinwalt <aljoscha@rheinwalt.berlin>
2016-06-14 14:44:33 +0200

    minor restruct into a more modular design

--- DemNets.c
+++ DemNets.c
@@ -6,14 +6,15 @@
 
 #define VERSION "0.1"
 
-static PyObject *
-slopes(const double *x, const double *y, const double *z, const unsigned int n, const unsigned int *tri, const unsigned int m) {
-	PyArrayObject *net, *slp;
+static PyArrayObject *
+network(const double *z, const unsigned int n, const unsigned int *tri, const unsigned int m) {
+	PyArrayObject *net;
 	npy_intp *dim;
 	unsigned int i, j, k, l;
 	unsigned int *lst[n], *lp, *e;
 	unsigned int u, v, w, vex, wex;
 	
+	// alloc list of arrays
 	for(i = 0; i < n; i++) {
 		lp = malloc(8 * sizeof(unsigned int));
 		if(!lp) {
@@ -24,13 +25,15 @@ slopes(const double *x, const double *y, const double *z, const unsigned int n, 
 		lp[1] = 2;
 		lst[i] = lp;
 	}
+
+	// retrieve downwards pointing links from the triangulation
 	l = 0;
 	for(i = 0; i < 3*m; i += 3) {
 		for(j = 0; j < 3; j++) {
 			u = tri[i+j];
 			v = tri[i+(j+1)%3];
 			w = tri[i+(j+2)%3];
-			if(z[v] > z[u] && z[w] > z[u])
+			if(z[v] >= z[u] && z[w] >= z[u])
 				continue;
 			lp = lst[u];
 			vex = wex = 0;
@@ -40,11 +43,11 @@ slopes(const double *x, const double *y, const double *z, const unsigned int n, 
 				if(lp[k] == w)
 					wex = 1;
 			}
-			if(!vex && z[v] <= z[u]) {
+			if(!vex && z[v] < z[u]) {
 				lp[lp[1]++] = v;
 				l++;
 			}
-			if(!wex && z[w] <= z[u]) {
+			if(!wex && z[w] < z[u]) {
 				lp[lp[1]++] = w;
 				l++;
 			}
@@ -54,16 +57,19 @@ slopes(const double *x, const double *y, const double *z, const unsigned int n, 
 			}
 		}
 	}
+
+	// alloc numpy array
 	dim = malloc(sizeof(npy_intp));
 	dim[0] = l + n + 1;
 	net = (PyArrayObject *) PyArray_ZEROS(1, dim, PyArray_UINT, 0);
-	slp = (PyArrayObject *) PyArray_ZEROS(1, dim, PyArray_UINT, 0);
 	free(dim);
-	if(!net || !slp) {
+	if(!net) {
 		PyErr_SetString(PyExc_MemoryError, "...");
 		return NULL;
 	}
 	e = (unsigned int *) net->data;
+
+	// store in compressed row format
 	j = n + 1;
 	for(i = 0; i < n; i++) {
 		lp = lst[i];
@@ -74,41 +80,111 @@ slopes(const double *x, const double *y, const double *z, const unsigned int n, 
 		free(lp);
 	}
 	e[n] = j;
-	return Py_BuildValue("(OO)", net, slp);
+	return net;
+}
+
+static PyArrayObject *
+slopes(const double *x, const double *y, const double *z, const unsigned int *net) {
+	PyArrayObject *slp;
+	npy_intp *dim;
+	unsigned i, j, k, n;
+	double *s, dx, dy;
+
+	// alloc numpy array
+	n = net[0] - 1;
+	dim = malloc(sizeof(npy_intp));
+	dim[0] = net[n];
+	slp = (PyArrayObject *) PyArray_ZEROS(1, dim, PyArray_DOUBLE, 0);
+	free(dim);
+	if(!slp) {
+		PyErr_SetString(PyExc_MemoryError, "...");
+		return NULL;
+	}
+	s = (double *) slp->data;
+
+	// estimate slopes along links
+	for(i = 0; i < n; i++) {
+		for(k = net[i]; k < net[i+1]; k++) {
+			j = net[k];
+			dx = x[i] - x[j];
+			dy = y[i] - y[j];
+			s[k] = (z[i] - z[j]) / sqrt(dx*dx + dy*dy);
+		}
+	}
+	return slp;
+}
+
+static PyObject *
+DemNets_FlowNetwork(PyObject *self, PyObject* args) {
+	PyObject *elearg, *triarg;
+	PyArrayObject *ele, *tri, *net;
+	unsigned int n;
+
+	// parse input
+	if(!PyArg_ParseTuple(args, "OO", &elearg, &triarg))
+		return NULL;
+	ele = (PyArrayObject *) PyArray_ContiguousFromObject(elearg, PyArray_DOUBLE, 1, 1);
+	tri = (PyArrayObject *) PyArray_ContiguousFromObject(triarg, PyArray_UINT, 2, 2);
+	if(!ele || !tri)
+		return NULL;
+
+	// retrieve flow network from the triangulation and elevation
+	net = network((double *)ele->data, ele->dimensions[0], (unsigned int *)tri->data, tri->dimensions[0]);
+	Py_DECREF(ele);
+	Py_DECREF(tri);
+	return PyArray_Return(net);
 }
 
 static PyObject *
 DemNets_Slopes(PyObject *self, PyObject* args) {
-	PyObject *xarg, *yarg, *zarg, *triarg, *SlopeNet;
-	PyArrayObject *x, *y, *z, *tri;
-	unsigned int n;
+	PyObject *xarg, *yarg, *zarg, *netarg;
+	PyArrayObject *x, *y, *z;
+	PyArrayObject *net, *slp;
+	unsigned int *e, n;
 
-	if(!PyArg_ParseTuple(args, "OOOO", &xarg, &yarg, &zarg, &triarg))
+	// parse input
+	if(!PyArg_ParseTuple(args, "OOOO", &xarg, &yarg, &zarg, &netarg))
 		return NULL;
 	x = (PyArrayObject *) PyArray_ContiguousFromObject(xarg, PyArray_DOUBLE, 1, 1);
 	y = (PyArrayObject *) PyArray_ContiguousFromObject(yarg, PyArray_DOUBLE, 1, 1);
 	z = (PyArrayObject *) PyArray_ContiguousFromObject(zarg, PyArray_DOUBLE, 1, 1);
-	tri = (PyArrayObject *) PyArray_ContiguousFromObject(triarg, PyArray_UINT, 2, 2);
-	if(!x || !y || !z || !tri)
+	net = (PyArrayObject *) PyArray_ContiguousFromObject(netarg, PyArray_UINT, 1, 1);
+	if(!x || !y || !z || !net)
 		return NULL;
+
+	// check input
 	n = x->dimensions[0];
-	if(n != y->dimensions[0])
+	if(n != y->dimensions[0]) {
+		PyErr_SetString(PyExc_IndexError, "(x, y) not of the same dimension.");
 		return NULL;
-	if(n != z->dimensions[0])
+	}
+	if(n != z->dimensions[0]) {
+		PyErr_SetString(PyExc_IndexError, "(x, y, z) not of the same dimension.");
 		return NULL;
-	
-	SlopeNet = slopes((double *)x->data, (double *)y->data, (double *)z->data, n,
-					  (unsigned int *)tri->data, tri->dimensions[0]);
+	}
+	e = (unsigned int *) net->data;
+	if(n != e[0] - 1) {
+		PyErr_SetString(PyExc_IndexError, "(x, y, z) does not match with the network.");
+		return NULL;
+	}
+	if(e[n] != net->dimensions[0]) {
+		PyErr_SetString(PyExc_IndexError, "corrupted network format.");
+		return NULL;
+	}
+
+	// get slopes
+	slp = slopes((double *)x->data, (double *)y->data, (double *)z->data, e);
+
 	Py_DECREF(x);
 	Py_DECREF(y);
 	Py_DECREF(z);
-	Py_DECREF(tri);
-	return SlopeNet;
+	Py_DECREF(net);
+	return PyArray_Return(slp);
 }
 
 static PyMethodDef DemNets_methods[] = {
-	{"Slopes", DemNets_Slopes, METH_VARARGS,
-	 "..."},
+	{"FlowNetwork", DemNets_FlowNetwork, METH_VARARGS, "..."},
+	{"Slopes", DemNets_Slopes, METH_VARARGS, "..."},
 	{NULL, NULL, 0, NULL}
 };
 
--- visuals.py
+++ visuals.py
@@ -1,37 +1,50 @@
+import sys
 from numpy.random import uniform, seed
 from matplotlib.mlab import griddata
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import Delaunay
-from DemNets import Slopes
+from DemNets import *
 
 # point cloud data
-seed(0)
-npts = 1000
+#seed(0)
+npts = 10000
 x = uniform(-2, 2, npts)
 y = uniform(-2, 2, npts)
 z = x*np.exp(-x**2 - y**2)
+tri = Delaunay(np.transpose((x, y)))
+net = FlowNetwork(z, tri.simplices.astype('uint32'))
+slp = Slopes(x, y, z, net)
+slp = slp.astype('float') / slp.max()
+#np.savetxt(sys.stdout, slp, fmt = '%.3f')
+
 
 # grid data for visualisation
 xi = np.linspace(-2.1, 2.1, 100)
 yi = np.linspace(-2.1, 2.1, 200)
 zi = griddata(x, y, z, xi, yi, interp = 'linear')
 zimax = abs(zi).max()
-CS = plt.contour(xi, yi, zi, 20, linewidths = 0.5, colors = 'k')
-CS = plt.contourf(xi, yi, zi, 10, cmap = plt.cm.rainbow,
-                  vmax=zimax, vmin=-zimax)
+plt.contour(xi, yi, zi, 20, linewidths = 0.5, colors = 'k')
+plt.contourf(xi, yi, zi, 10, cmap = plt.cm.rainbow,
+             vmax=zimax, vmin=-zimax)
 plt.colorbar()
 #plt.scatter(x, y, marker = 'o', c = 'b', s = 5)
 
-tri = Delaunay(np.transpose((x, y)))
-net, slp = Slopes(x, y, z, tri.simplices.astype('uint32'))
 for i in xrange(npts):
     for k in xrange(net[i], net[i+1]):
-        dx = 0.9 * (x[net[k]] - x[i])
-        dy = 0.9 * (y[net[k]] - y[i])
-        plt.arrow(x[i], y[i], dx, dy, length_includes_head = True, shape = 'left')
+        j = net[k]
+        dx = (x[j] - x[i]) * 0.9
+        dy = (y[j] - y[i]) * 0.9
+        plt.arrow(x[i], y[i], dx, dy,
+                  length_includes_head = True, shape = 'left')
+#                  width = 0.01 * slp[k], head_width = 0.003)
 
 plt.xlim(-2, 2)
 plt.ylim(-2, 2)
 plt.title('griddata test (%d points)' % npts)
+plt.show()
+
+sys.exit()
+CS = plt.contour(xi, yi, zi, 20, linewidths = 0.5, colors = 'k')
+plt.quiver(xi, yi, ui, vi)
 plt.show()
