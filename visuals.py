c7821a68b4eaed04e00d84bf1bcd1def6e5191bf
Aljoscha Rheinwalt <aljoscha@rheinwalt.berlin>
2016-06-16 10:01:41 +0200

    visuals

--- visuals.py
+++ visuals.py
@@ -2,13 +2,18 @@ import sys
 from numpy.random import uniform, seed
 from matplotlib.mlab import griddata
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
 import numpy as np
 from scipy.spatial import Delaunay
 from DemNets import *
 
+norm  = colors.Normalize(vmin = 0, vmax = 1)
+sccm = cmx.ScalarMappable(norm = norm, cmap = plt.cm.hot_r)
+
 # point cloud data
 #seed(0)
-npts = 10000
+npts = 100
 x = uniform(-2, 2, npts)
 y = uniform(-2, 2, npts)
 z = x*np.exp(-x**2 - y**2)
@@ -16,14 +21,15 @@ tri = Delaunay(np.transpose((x, y)))
 net = FlowNetwork(z, tri.simplices.astype('uint32'))
 slp = Slopes(x, y, z, net)
 slp = slp.astype('float') / slp.max()
-#np.savetxt(sys.stdout, slp, fmt = '%.3f')
-
 
 # grid data for visualisation
 xi = np.linspace(-2.1, 2.1, 100)
 yi = np.linspace(-2.1, 2.1, 200)
 zi = griddata(x, y, z, xi, yi, interp = 'linear')
 zimax = abs(zi).max()
+
+# figure
+plt.figure(1, (19.2, 12))
 plt.contour(xi, yi, zi, 20, linewidths = 0.5, colors = 'k')
 plt.contourf(xi, yi, zi, 10, cmap = plt.cm.rainbow,
              vmax=zimax, vmin=-zimax)
@@ -36,15 +42,11 @@ for i in xrange(npts):
         dx = (x[j] - x[i]) * 0.9
         dy = (y[j] - y[i]) * 0.9
         plt.arrow(x[i], y[i], dx, dy,
-                  length_includes_head = True, shape = 'left')
-#                  width = 0.01 * slp[k], head_width = 0.003)
+                  length_includes_head = True, shape = 'left',
+                  color = sccm.to_rgba(slp[k]))
 
 plt.xlim(-2, 2)
 plt.ylim(-2, 2)
-plt.title('griddata test (%d points)' % npts)
-plt.show()
-
-sys.exit()
-CS = plt.contour(xi, yi, zi, 20, linewidths = 0.5, colors = 'k')
-plt.quiver(xi, yi, ui, vi)
-plt.show()
+plt.title('Visuals (%d points)' % npts)
+plt.tight_layout()
+plt.savefig('%s.png' % sys.argv[0][:-3])
