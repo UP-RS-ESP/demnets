7784a0ceda07b1d496ab936be4e77befb3c3d931
Aljoscha Rheinwalt <aljoscha@rheinwalt.berlin>
2016-06-20 11:45:49 +0200

    show corresponding voronoi cells

--- visuals.py
+++ visuals.py
@@ -1,40 +1,41 @@
 import sys
 from numpy.random import uniform, seed
-from matplotlib.mlab import griddata
+#from matplotlib.mlab import griddata
+from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
 import numpy as np
-from scipy.spatial import Delaunay
+from scipy.spatial import Delaunay, Voronoi, voronoi_plot_2d
 from DemNets import *
 
 norm  = colors.Normalize(vmin = 0, vmax = 1)
 sccm = cmx.ScalarMappable(norm = norm, cmap = plt.cm.hot_r)
+cmap = plt.cm.rainbow
 
 # point cloud data
 #seed(0)
 npts = 100
-x = uniform(-2, 2, npts)
-y = uniform(-2, 2, npts)
+pts = uniform(-2, 2, (npts, 2))
+x, y = pts[:,0], pts[:,1]
 z = x*np.exp(-x**2 - y**2)
-tri = Delaunay(np.transpose((x, y)))
+tri = Delaunay(pts)
 net = FlowNetwork(z, tri.simplices.astype('uint32'))
 slp = Slopes(x, y, z, net)
 slp = slp.astype('float') / slp.max()
 
 # grid data for visualisation
-xi = np.linspace(-2.1, 2.1, 100)
-yi = np.linspace(-2.1, 2.1, 200)
-zi = griddata(x, y, z, xi, yi, interp = 'linear')
+xi, yi = np.mgrid[-2:2:800j, -2:2:800j]
+zi = griddata(pts, z, (xi, yi), method = 'nearest')
 zimax = abs(zi).max()
 
 # figure
 plt.figure(1, (19.2, 12))
-plt.contour(xi, yi, zi, 20, linewidths = 0.5, colors = 'k')
-plt.contourf(xi, yi, zi, 10, cmap = plt.cm.rainbow,
-             vmax=zimax, vmin=-zimax)
-plt.colorbar()
-#plt.scatter(x, y, marker = 'o', c = 'b', s = 5)
+plt.imshow(zi.T, interpolation = 'nearest',
+           cmap = cmap, origin = 'lower',
+           aspect = 'auto', extent = [-2,2,-2,2])
+cb = plt.colorbar()
+cb.set_label('Elevation')
 
 for i in xrange(npts):
     for k in xrange(net[i], net[i+1]):
@@ -45,8 +46,6 @@ for i in xrange(npts):
                   length_includes_head = True, shape = 'left',
                   color = sccm.to_rgba(slp[k]))
 
-plt.xlim(-2, 2)
-plt.ylim(-2, 2)
-plt.title('Visuals (%d points)' % npts)
+plt.title('DEM point cloud of %i points\n(arrows are colored according to steepness)' % npts)
 plt.tight_layout()
 plt.savefig('%s.png' % sys.argv[0][:-3])
